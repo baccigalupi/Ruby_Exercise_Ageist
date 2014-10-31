a5630a7cf813acae73da9a64f1859230e9d27538
Kane Baccigalupi <baccigalupi@gmail.com>
2014-10-31 15:03:26 -0700

    Feedback
    Good work

--- ageist.rb
+++ ageist.rb
@@ -1,6 +1,7 @@
-age = rand(0..110)
+age = rand(0..110) # never seen this way of calling rand with a range!!
+# make sure you go up to 111, otherwise you will never get to the top of the range
 
-string = ''
+string = '' # this shouldn't be necessary since you created a local variable in each branch
 if age <= 1
   string = 'baby'
 
@@ -26,4 +27,5 @@ else
   string = 'record breaking'
 end
 
+# You could also use string interpolation here!
 puts 'Age is ' + age.to_s + ' ' + 'and you are a ' + string + '.'
